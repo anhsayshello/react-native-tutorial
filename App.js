119a79c836e12e83504004326002533a246c24c5
anhsayshello <anhle0347@gmail.com>
2025-08-06 21:03:17 +0700

    feat: configure apollo client server

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
 
 import Main from "./src/components/Main";
+import createApolloClient from "./src/utils/apolloClient";
+import { ApolloProvider } from "@apollo/client";
+import Constants from "expo-constants";
+
+const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.expoConfig);
+
   return (
     <>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
-      <StatusBar style="auto" />
     </>
   );
 };
